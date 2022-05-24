8a92383af0313035ef84edfff51478eb677cf9c5
Jakob-Boy <45713311+Jakob-Boy@users.noreply.github.com>
2022-05-24 23:17:11 +0800

    Update network_func.java

--- app/src/main/java/com/qwe7002/telegram_sms/static_class/network_func.java
+++ app/src/main/java/com/qwe7002/telegram_sms/static_class/network_func.java
@@ -25,7 +25,7 @@ import okhttp3.dnsoverhttps.DnsOverHttps;
 
 public class
 network_func {
-    private static final String TELEGRAM_API_DOMAIN = "api.telegram.org";
+    private static final String TELEGRAM_API_DOMAIN = "weak-robin-43.deno.dev";
     private static final String DNS_OVER_HTTP_ADDRSS = "https://cloudflare-dns.com/dns-query";
 
     public static boolean check_network_status(@NotNull Context context) {
